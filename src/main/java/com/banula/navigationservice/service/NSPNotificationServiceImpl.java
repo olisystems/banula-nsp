d13e2f351b7c7cf5a0f47289bf791c8f97a2b3f2
Matheus Rosendo <matheus.rosendo@my-oli.com>
2026-06-11 13:46:32 -0300

    refactor: simplify NSPNotificationServiceImpl to skip broadcasting hub client info updates

--- src/main/java/com/banula/navigationservice/service/NSPNotificationServiceImpl.java
+++ src/main/java/com/banula/navigationservice/service/NSPNotificationServiceImpl.java
@@ -1,40 +1,19 @@
 package com.banula.navigationservice.service;
 
-import com.banula.navigationservice.config.ApplicationConfiguration;
-import com.banula.openlib.ocn.client.OcnClient;
-import com.banula.openlib.ocpi.model.ClientInfo;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.banula.openlib.ocpi.model.ClientInfo;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-@AllArgsConstructor
 public class NSPNotificationServiceImpl implements NSPNotificationService {
-
-    private final ApplicationConfiguration applicationConfiguration;
-    private final OcnClient ocnClient;
 
     @Override
     public void broadcastHubClientInfoUpdate(ClientInfo clientInfo) {
-        try {
-            String outflowUrl = applicationConfiguration.getPlatformUrl() + "/ocpi/outflow/ocpi/2.2/hubclientinfo";
-            ocnClient.executeOcpiOperation(
-                    outflowUrl,
-                    clientInfo,
-                    "",
-                    "",
-                    new ParameterizedTypeReference<>() {
-                    },
-                    HttpMethod.PUT,
-                    List.of());
-        } catch (Exception e) {
-            log.debug("Error broadcasting client info party update {} ({}): {}",
-                    clientInfo.getPartyId(), clientInfo.getCountryCode(), e.getMessage());
-        }
+        // NSP only accepts incoming requests and does not broadcast updates
+        log.debug("Broadcast hub client info update called for {} ({}): skipped - NSP is receive-only",
+                clientInfo.getPartyId(), clientInfo.getCountryCode());
     }
 }
